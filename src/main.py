0d830f03c58ef9188d4ec83b7214d795f89b6bdc
Jessy Latmi <jessy.latmi@etu.umontpellier.fr>
2026-04-01 16:15:34 +0200

    fix init

--- src/main.py
+++ src/main.py
@@ -2,11 +2,11 @@ from fastapi import FastAPI
 import uvicorn
 import os
 from dotenv import load_dotenv
-from routes.city import router
+from routes.todos import router
 from database import init_db
 
 load_dotenv()
-port = os.environ["PORT"]
+port = int(os.environ["PORT"])
 app = FastAPI()
 
 init_db()
@@ -14,4 +14,4 @@ init_db()
 app.include_router(router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=port, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=port)
